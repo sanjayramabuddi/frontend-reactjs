afc2cda9eb87e6e4aa04b8e9ab668b6b38e3b360
sanjayramabuddi <sanjayramabuddi@gmail.com>
2024-03-20 00:02:57 +0530

    removed api key

--- react-firebase/src/firebaseConfig.js
+++ react-firebase/src/firebaseConfig.js
@@ -6,7 +6,7 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB-UTNfZq5Pt5ajU8UrNvG4PjuaTAk3058",
+  apiKey: "AddApiKeyHere",
   authDomain: "react-firebase-7443b.firebaseapp.com",
   projectId: "react-firebase-7443b",
   storageBucket: "react-firebase-7443b.appspot.com",
